417bddd38a48fc989661e06fd8771fa401df44d7
RohMad777 <rohmadrock@gmail.com>
2021-08-09 22:37:13 +0700

    gunicorn

--- app.py
+++ app.py
@@ -2,16 +2,16 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-# Create flask app
-flask_app = Flask(__name__)
+# Create app
+app = Flask(__name__)
 model = pickle.load(open("model/model_rf_clf_rev.pkl", "rb"))
 
 
-@flask_app.route("/")
+@app.route("/")
 def Home():
     return render_template("home.html")
 
-@flask_app.route("/predict", methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
     float_features = [float(x) for x in request.form.values()]
     features = [np.array(float_features)]
@@ -25,15 +25,15 @@ def predict():
             output[prediction[0]]))
 
 
-@flask_app.route('/data-set')
+@app.route('/data-set')
 def dataSet():
     return render_template('bank_dataset_filt.html')
 
 
-@flask_app.route('/predict')
+@app.route('/predict')
 def predicts():
     return render_template('predict.html')
 
 
 if __name__ == "__main__":
-    flask_app.run(debug=True)
+    app.run(debug=True)
